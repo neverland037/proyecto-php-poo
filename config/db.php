466d12625c2ac099f100155cbef3d48c64969eb4
neverland037 <93451798+neverland037@users.noreply.github.com>
2023-12-16 22:50:54 -0600

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 class Database{
 	public static function connect(){
-		$db = new mysqli('localhost:3307', 'root', '', 'tienda_master');
+		$db = new mysqli('159.89.38.223', 'root', 'TheBeast@037dio', 'ventas');
 		$db->query("SET NAMES 'utf8'");
 		return $db;
 	}
